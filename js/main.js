f8ba67c5bbba6e89eef4563141f93a636c47fa57
Yaryi <kinomir-v@yandex.ru>
2024-05-27 16:21:29 +0300

    third

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+// const swiper1 = new Swiper('.swiper', {
+//   speed: 800,
+//   autoplay: {
+//     delay: 50,
+//   },
+
+//   direction: 'vertical',
+//   loop: true,
+//   slidesPerView: 2,
+//   spaceBetween: 14,
+// });
